f37a81c550e7078633afaaadd1024760a2a7c2f3
JO <jose.sat94@gmail.com>
2023-03-08 23:42:32 -0300

    entrega-3

---
+++ src/ProductManager.js
@@ -0,0 +1,70 @@
+//Importación de la libreria File System
+const fs = require('fs');
+
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+  }
+
+//Agregar un producto al archivo
+  addProduct(product) {
+    const products = this.getProductsArchivo();
+    product.id = products.length + 1;
+    products.push(product);
+    this.saveProductsArchivo(products);
+  }
+
+  // Obtener los productos del archivo
+  getProducts() {
+    return this.getProductsArchivo();
+  }
+
+//Obtener un producto por id del archivo
+  getProductById(id) {
+    const products = this.getProductsArchivo();
+    const product = products.find(p => p.id == id);    
+
+    return product || null;
+  }
+
+//Actualizar un producto en el archivo
+  updateProduct(id, updatedProduct) {
+    const products = this.getProductsArchivo();
+
+    const productIndex = products.findIndex(p => p.id === id);
+
+    if (productIndex !== -1) {
+      products[productIndex] = Object.assign({}, products[productIndex], updatedProduct, {id});
+      this.saveProductsArchivo(products);
+    }
+  }
+
+//Eliminar un producto del archivo  
+  deleteProduct(id) {
+    const products = this.getProductsArchivo();
+    const filteredProducts = products.filter(p => p.id !== id);
+
+    this.saveProductsArchivo(filteredProducts);
+  }
+
+  getProductsArchivo() {
+    try {
+        if(!fs.existsSync(this.path)){
+            fs.writeFileSync(this.path, "[]");
+        }
+        const productsData = fs.readFileSync(this.path, 'utf-8');
+        const products = JSON.parse(productsData);
+        return products;
+        }
+    catch (error){
+      console.log(error)
+      return [];
+    }
+  }
+
+  saveProductsArchivo(products) {
+    fs.writeFileSync(this.path, JSON.stringify(products));
+  }
+}
+
+module.exports = ProductManager
---
+++ src/index.js
@@ -0,0 +1,47 @@
+//importamos la dependencia express
+const express = require('express')
+
+//creamos la variable app y esta ejecuta express
+const app = express()
+
+//configurar el servidor para que reciba datos complejos
+app.use(express.urlencoded({extended:true}))
+
+//guardamos el puerto en una variable
+const port = 3001
+
+//importamos la class ProductManager y el json de productos
+const ProductManager = require("./ProductManager")
+const productManager = new ProductManager("./products.json")
+
+//consulta por productos
+app.get('/products', async (req, res) => {
+    try {
+      const limit = req.query.limit;
+      const products = await productManager.getProducts();
+      const result = limit ? products.slice(0, limit) : products;
+      res.json(result);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+
+//consulta por productos por id
+app.get('/products/:pid', async (req, res) => {
+    try {
+      const { pid } = req.params;
+      const product = await productManager.getProductById(pid);
+      if (!product) {
+        res.status(404).json({ error: 'Producto no encontrado' });
+      } else {
+        res.json(product);
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+app.listen(port, () => {
+    console.log(`Se está ejecutando en el puerto ${port}`)
+})
